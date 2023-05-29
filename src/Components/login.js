b838029776a0f9f9917f7709067419cdf3b65ad4
Vani Awasthi <awasthivani95@gmail.com>
2023-05-29 12:18:38 +0530

    Add existing project files to Git

--- src/Components/login.js
+++ src/Components/login.js
@@ -1,12 +1,21 @@
 import React from 'react'
+import { useForm } from "react-hook-form"
 
-function login() {
+function Login() {
+    const {register, handleSubmit, formState: { errors } } = useForm();
+    const onSubmit = (data) => console.log(data);
   return (
     <div>
-      <div>
-        
-      </div>
+        <p className="title">Registration Form</p>
+        <form className="App" onSubmit={handleSubmit(onSubmit)}>
+            <input type="text" {...register("name")} />
+            <input type="email" {...register("email", { required: true })} />
+            {errors.email && <span style={{ color: "red" }}>
+            *Email* is mandatory </span>}
+            <input type="password" {...register("password")} />
+            <input type={"submit"} style={{ backgroundColor: "#a1eafb" }} />
+        </form>
     </div>
   )
 }
-export default login
+export default Login
