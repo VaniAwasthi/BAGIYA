28aafc1edd4c563da06f25556e1aeebe55eea627
Vani Awasthi <awasthivani95@gmail.com>
2023-05-29 13:58:59 +0530

    adding some changes

--- src/Components/login.js
+++ src/Components/login.js
@@ -1,9 +1,28 @@
 import React from 'react'
-import { useForm } from "react-hook-form"
+import { useForm } from "react-hook-form";
+import './login.css'
 
 function Login() {
-    const {register, handleSubmit, formState: { errors } } = useForm();
-    const onSubmit = (data) => console.log(data);
+    
+    const {
+        register,
+        handleSubmit,
+        formState: { errors },
+      } = useForm();
+      
+      const onSubmit = (data) => {
+        const userData = JSON.parse(localStorage.getItem(data.email));
+        if (userData) { // getItem can return actual value or null
+          if (userData.password === data.password) {
+            console.log(userData.name + " You Are Successfully Logged In");
+          } else {
+            console.log("Email or Password is not matching with our record");
+          }
+        } else {
+          console.log("Email or Password is not matching with our record");
+        }
+      };
+
   return (
     <div>
         <p className="title">Registration Form</p>
