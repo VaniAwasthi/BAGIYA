13a7a16948d4ab7246b393e451e8420a8c90febd
Vani Awasthi <awasthivani95@gmail.com>
2023-06-07 11:54:21 +0530

    Adding new section

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -2,6 +2,7 @@ import React from "react";
 import { Link, useMatch, useResolvedPath } from "react-router-dom";
 import "./Footer.css";
 import logo from "./Image/logo.png";
+import { FaFacebook, FaInstagram, FaTwitter, FaLinkedin } from "react-icons/fa";
 
 const Footer = () => {
   const year = new Date().getFullYear();
@@ -18,12 +19,76 @@ const Footer = () => {
               <h4>BAGIYA</h4>
             </Link>
           </div>
+          <div className="FooterContent">
+            <h3> More About Company</h3>
+            <p>
+              We are a team of horticultural experts, logistical masterminds,
+              design geniuses, and lots of dedicated worker bees trying our best
+              to earn the smile you wear on seeing the healthy, happy plants you
+              receive
+            </p>
+          </div>
         </div>
-        <div className="secondsectiom"></div>
+        <div className="secondsectiom">
+          <h3>Keep Connected</h3>
+          {/* <ul>
+            <li>
+              <FaFacebook style={{ color: "black" }} />
+              Facebook
+            </li>
+            <li>
+              <FaInstagram style={{ color: "black" }} />
+              FaInstagram
+            </li>
+            <li>
+              <FaTwitter style={{ color: "black" }} />
+              Twitter
+            </li>
+            <li>
+              <FaLinkedin style={{ color: "black" }} />
+              Linkedin
+            </li>
+          </ul> */}
+          <div className="IconContainer">
+            <div className="Icons">
+              <div className="Icon-Items">
+                <FaFacebook />
+              </div>
+              <div className="Icon-Items">
+                <FaInstagram />
+              </div>
+              <div className="Icon-Items">
+                <FaTwitter />
+              </div>
+              <div className="Icon-Items">
+                <FaLinkedin />
+              </div>
+            </div>
+            <div className="Icons">
+              <div className="Icon-Item">
+                <p>Facebook</p>
+              </div>
+              <div className="Icon-Item">
+                <p>Instagram</p>
+              </div>
+              <div className="Icon-Item">
+                <p>Twitter</p>
+              </div>
+              <div className="Icon-Item">
+                <p>Linkedin</p>
+              </div>
+            </div>
+          </div>
+        </div>
         <div className="ThirdSection"></div>
-        <div className="fourth section"></div>
+        <div></div>
       </div>
-      {`Copyright © BAGIYA  ${year}`}
+      <div className="CopyrightSection">
+        <div>{`Copyright © BAGIYA  ${year}`}</div>
+        <div>
+          <p>Privacy Policy | Terms and Conditions | FAQ </p>
+        </div>
+      </div>
     </footer>
   );
 };
