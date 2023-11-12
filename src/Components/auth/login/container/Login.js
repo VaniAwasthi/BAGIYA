83c25ac7c0ca8e122872f3636bbc04ed16821ebb
Vani Awasthi <awasthivani95@gmail.com>
2023-11-12 20:41:09 +0530

    initial commit

--- src/Components/auth/login/container/Login.js
+++ src/Components/auth/login/container/Login.js
@@ -1,21 +1,26 @@
-import React from 'react'
-import { useForm } from "react-hook-form"
+import React from "react";
+import { useForm } from "react-hook-form";
 
-function Login() {
-    const {register, handleSubmit, formState: { errors } } = useForm();
-    const onSubmit = (data) => console.log(data);
+const Login = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+  const onSubmit = (data) => console.log(data);
   return (
     <div>
-        <p className="title">Registration Form</p>
-        <form className="App" onSubmit={handleSubmit(onSubmit)}>
-            <input type="text" {...register("name")} />
-            <input type="email" {...register("email", { required: true })} />
-            {errors.email && <span style={{ color: "red" }}>
-            *Email* is mandatory </span>}
-            <input type="password" {...register("password")} />
-            <input type={"submit"} style={{ backgroundColor: "#a1eafb" }} />
-        </form>
+      <p className="title">Registration Form</p>
+      <form className="App" onSubmit={handleSubmit(onSubmit)}>
+        <input type="text" {...register("name")} />
+        <input type="email" {...register("email", { required: true })} />
+        {errors.email && (
+          <span style={{ color: "red" }}>*Email* is mandatory </span>
+        )}
+        <input type="password" {...register("password")} />
+        <input type={"submit"} style={{ backgroundColor: "#a1eafb" }} />
+      </form>
     </div>
-  )
-}
-export default Login
+  );
+};
+export default Login;
